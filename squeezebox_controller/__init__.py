34e4387d2dd477cb6e63901aa0fd852a61870398
Samuel Russell <sr14978@bristol.ac.uk>
2020-05-09 17:46:07 +0100

    version update

--- squeezebox_controller/__init__.py
+++ squeezebox_controller/__init__.py
@@ -69,6 +69,7 @@ search_types = {
 }
 
 queries = {
+  "RAW": lambda info: info,
   "VOLUME": lambda info: "The volume is at %d percent"%(info['mixer volume']),
   "NOW PLAYING": lambda info: info['playlist_loop'][0]['title'] + ' by ' + info['playlist_loop'][0]['artist'] \
                       if 'artist' in info['playlist_loop'][0] else info['playlist_loop'][0]['title'] \
